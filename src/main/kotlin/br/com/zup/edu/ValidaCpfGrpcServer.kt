1253a65e7c8ba6c6c93cb30c9cde9374c3227475
jordihofc <jordi.silva@ufv.br>
2021-05-13 10:40:59 -0300

    tratando erros em protobuff, adicionando meta-dados na responses

--- src/main/kotlin/br/com/zup/edu/ValidaCpfGrpcServer.kt
+++ src/main/kotlin/br/com/zup/edu/ValidaCpfGrpcServer.kt
@@ -1,5 +1,11 @@
 package br.com.zup.edu
 
+
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import com.google.rpc.StatusProto
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
@@ -9,28 +15,43 @@ class ValidaCpfGrpcServer : ValidacpfServiceGrpc.ValidacpfServiceImplBase() {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun valida(request: ValidacpfRequest?, responseObserver: StreamObserver<ValidacpfResponse>?) {
-        var cpf:String? = request?.cpf
+        var cpf: String? = request?.cpf
         logger.info("CPF: $cpf")
         if (request?.cpf != null) {
-            val valido = validaCpf(request.cpf)
-            if (valido) {
-                var response = ValidacpfResponse.newBuilder()
-                    .setCpf(cpf)
-                    .setMensagem("Cpf é válido")
-                    .build()
-                responseObserver!!.onNext(response)
-                responseObserver.onCompleted()
-                return;
+            if (request.cpf.matches("[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}".toRegex()) || request.cpf.matches("[0-9]{11}".toRegex())) {
+                val valido = validaCpf(request.cpf)
+                if (valido) {
+                    var response = ValidacpfResponse.newBuilder()
+                        .setCpf(cpf)
+                        .setMensagem("Cpf é válido")
+                        .build()
+                    responseObserver!!.onNext(response)
+                    responseObserver.onCompleted()
+                    return;
+                }
+            } else if (cpf!!.isBlank()) {
+                var statusProto = com.google.rpc.Status.newBuilder()
+                    .setCode(Code.INVALID_ARGUMENT_VALUE)
+                    .setMessage("Valor de cpf não pode ser nulo").addDetails(
+                        Any.pack(
+                            ErrorDetails
+                                .newBuilder()
+                                .setCode(400)
+                                .setMessage("BAD REQUEST").build()
+                        )
+                    ).build()
+
+                var e = io.grpc.protobuf.StatusProto.toStatusRuntimeException(statusProto)
+                responseObserver!!.onError(e)
+
+            } else {
+                var e = Status.INVALID_ARGUMENT
+                    .withDescription("valor informado nao corresponde a um cpf")
+                    .asRuntimeException()
+                responseObserver!!.onError(e)
             }
         }
 
-        cpf = "INVALID OR NULL"
-        var response = ValidacpfResponse.newBuilder()
-            .setCpf(cpf)
-            .setMensagem("Invalid")
-            .build()
-        responseObserver!!.onNext(response)
-        responseObserver.onCompleted()
 
     }
 
--- src/main/kotlin/br/com/zup/edu/Validator.kt
+++ src/main/kotlin/br/com/zup/edu/Validator.kt
@@ -1,81 +1,92 @@
 package br.com.zup.edu
 
-public fun validaCpf(cpf:String): Boolean {
+public fun validaCpf(cpf: String): Boolean {
     val verificadoresPrimeiroDigito = arrayListOf(10, 9, 8, 7, 6, 5, 4, 3, 2)
     val verificadoresSegundoDigito = arrayListOf(11, 10, 9, 8, 7, 6, 5, 4, 3, 2)
-    val digitoverficadorOne = cpf.substring(12, 13)
-    val one = retornainteiro(digitoverficadorOne[digitoverficadorOne.length - 1])
-    val digitoverficadortwo = cpf.substring(13, 14)
-    val two = retornainteiro(digitoverficadortwo[digitoverficadortwo.length - 1])
+    var primeiroDigito: String = ""
+    var segundoDigito: String = ""
+    var digitoverficadorOne: String? = null
+    var digitoverficadortwo: String? = null
+    var one: Int? = null
+    var two: Int? = null
+    if (cpf.matches("[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}".toRegex())) {
+        digitoverficadorOne = cpf.substring(12, 13)
+        digitoverficadortwo = cpf.substring(13, 14)
+        primeiroDigito = extrairMascaraParaPrimeiroDigito(cpf)
+        segundoDigito = extrairMascaraParaSegundoDigito(cpf)
+    } else {
+        digitoverficadorOne = cpf.substring(9, 10)
+        digitoverficadortwo = cpf.substring(10, 11)
+        primeiroDigito = cpf.substring(0,9)
+        segundoDigito = cpf.substring(0,10)
 
-    val primeiroDigito = cpf
-        .replace(".", "")
-        .replace("-", "").substring(0, 9)
+    }
+    one = retornainteiro(digitoverficadorOne[digitoverficadorOne.length - 1])
+    two = retornainteiro(digitoverficadortwo[digitoverficadortwo.length - 1])
+
+
     val digitos = toIntList(primeiroDigito)
-
+    val digitosVerificadorDois = toIntList(segundoDigito)
     var valor = 0
+
+
     digitos.forEachIndexed { index, i ->
         valor += i * verificadoresPrimeiroDigito[index]
     }
-
-    val segundoDigito = cpf
-        .replace(".", "")
-        .replace("-", "").substring(0, 10)
-
-    val digitosVerificadorDois = toIntList(segundoDigito)
 
     var valorDois = 0
     digitosVerificadorDois.forEachIndexed { index, i ->
         valorDois += i * verificadoresSegundoDigito[index]
     }
-    var validOneDigit=false
-    var validTwoDigit=false
-    if ((valor * 10) % 11 == one ) {
-        validOneDigit=true
-        println(valor)
+    var validOneDigit = false
+    var validTwoDigit = false
+    if ((valor * 10) % 11 == one) {
+        validOneDigit = true
     }
-    if ((valorDois * 10) % 11 == two){
-        validTwoDigit=true
-        println(valorDois)
+    if ((valorDois * 10) % 11 == two) {
+        validTwoDigit = true
     }
     return validOneDigit && validTwoDigit
 
 }
 
+private fun extrairMascaraParaPrimeiroDigito(cpf: String) = cpf
+    .replace(".", "")
+    .replace("-", "").substring(0, 9)
+
+private fun extrairMascaraParaSegundoDigito(cpf: String) = cpf
+    .replace(".", "")
+    .replace("-", "").substring(0, 10)
+
 private fun toIntList(replace: String): MutableList<Int> {
-    val a= mutableListOf<Int>()
+    val a = mutableListOf<Int>()
     replace.forEach {
-       a.add( retornainteiro(it))
+        a.add(retornainteiro(it))
     }
     return a;
 }
-fun retornainteiro(it: Char):Int{
+
+fun retornainteiro(it: Char): Int {
     if (it.equals('0')) {
-       return 0
+        return 0
     } else if (it.equals('1')) {
-       return 1
+        return 1
     } else if (it.equals('2')) {
-       return 2
+        return 2
     } else if (it.equals('3')) {
-       return 3
+        return 3
     } else if (it.equals('4')) {
-       return 4
+        return 4
     } else if (it.equals('5')) {
-       return 5
+        return 5
     } else if (it.equals('6')) {
-       return 6
+        return 6
     } else if (it.equals('7')) {
-       return 7
+        return 7
     } else if (it.equals('8')) {
-       return 8
+        return 8
     } else {
         return 9
     }
 
 }
-
-
-
-
-
-
